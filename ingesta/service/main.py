9c9602af574304271941534b6a6cafa5abaea7be
JefriVargas <143043024+JefriVargas@users.noreply.github.com>
2024-11-27 03:34:00 -0500

    Update main.py

--- ingesta/service/main.py
+++ ingesta/service/main.py
@@ -26,7 +26,7 @@ logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s - %(levelname)s - %(message)s",
     handlers=[
-        logging.FileHandler(log_file),
+        logging.FileHandler(log_file, mode="a"),
         logging.StreamHandler()
     ]
 )
